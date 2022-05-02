f660ab270d0d104c0b86d91124197f5cc71e0fa9
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-05-02 17:27:00 +0300

    add hw

---
+++ src/js/01-gallery.js
@@ -0,0 +1,33 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+//console.log(galleryItems);
+const paletteContainer = document.querySelector('.gallery');
+const cardMarkup = createGalleryItemsMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML('afterbegin', cardMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+</a>`;
+    }).join('');    
+    
+};
+
+paletteContainer.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {   
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+    evt.preventDefault();
+
+    let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+    console.log(gallery);
+}
+
