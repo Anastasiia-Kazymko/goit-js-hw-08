85402f9e9a6fd2ca80ffd059e52ca88b50191da0
Anastasiia8888 <92369437+Anastasiia8888@users.noreply.github.com>
2022-05-04 15:57:06 +0300

    push 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe); 
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.on('loaded', pageUpdate);
+
+    
+function onPlay (data) {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds)   
+};
+
+function pageUpdate() {
+    const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (!currentTime) {
+        return
+    } player.setCurrentTime(currentTime);
+};
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
